78d6baa453f00ee4e2511ad618f9065bf785a644
Tran Hoang Phuc <PhucTHCE182086@fpt.edu.vn>
2026-03-15 14:02:59 +0700

    fix ar page

--- app/ar/page.tsx
+++ app/ar/page.tsx
@@ -2,7 +2,7 @@
 export default function ProductPage() {
     return (
         <div className="w-full h-[600]">
-            <iframe width="100%" height="100%" allow="camera; fullscreen; autoplay" src="https://player.artivive.com/artworks/69b41004ee4d360782fce9de" frameborder="0"></iframe>
+            <iframe width="100%" height="100%" allow="camera; fullscreen; autoplay" src="https://player.artivive.com/artworks/69b41004ee4d360782fce9de"></iframe>
         </div>
     );
 }
